9161abc5edf6053885ede8aba2a37e7f2d2b8823
tjrandall21 <tjrandall21@gmail.com>
2025-10-14 18:34:08 -0700

    update

--- VGP230/01_HelloX/WinMain.cpp
+++ VGP230/01_HelloX/WinMain.cpp
@@ -1,18 +1,18 @@
 #include <XEngine.h>
 #include "BouncingSprite.h"
-BouncingSprite mario = BouncingSprite();
+BouncingSprite spriteCan = BouncingSprite();
 BouncingSprite qBlock = BouncingSprite();
 
 void GameInit()
 {
-	mario.Init("mario.png",X::GetScreenWidth()/4, X::GetScreenHeight()/4, -2.5f, -1.5f);
+	spriteCan.Init("sprite_can.png",X::GetScreenWidth()/4, X::GetScreenHeight()/4, -4.5f, -2.5f);
 	qBlock.Init("qmark.png", X::GetScreenWidth()*3 / 4, X::GetScreenHeight()*3 / 4, 3.0f, 5.0f);
 }
 
 bool GameLoop(float deltaTime)
 {
-	mario.Step();
-	mario.Draw();
+	spriteCan.Step();
+	spriteCan.Draw();
 	qBlock.Step();
 	qBlock.Draw();
 	return X::IsKeyPressed(X::Keys::ESCAPE);
